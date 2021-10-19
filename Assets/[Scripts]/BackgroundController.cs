8533c59bbe2f275fda1739a5e016ad36c579ed4d
Sam <samcpollock@gmail.com>
2021-10-19 16:33:10 -0400

    Changed background scrolling to right-to-left
    - Moved background images 
    - Added third background image 
    - Changed background scale

--- Assets/[Scripts]/BackgroundController.cs
+++ Assets/[Scripts]/BackgroundController.cs
@@ -1,11 +1,22 @@
-﻿using System.Collections;
+﻿/*
+ * Source File: BackgroundController.cs
+ * Student Name: Sam Pollock
+ * Student ID: 101279608
+ * Date Modified: October 19, 2021
+ * Description: Controls the scrolling background
+ * Last revision: Ver. 0.1 - Changed from top down scrolling to left from right scrolling.
+ 
+ */
+
+
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class BackgroundController : MonoBehaviour
 {
-    public float verticalSpeed;
-    public float verticalBoundary;
+    public float horizontalSpeed;
+    public float horizontalBoundary;
 
     // Update is called once per frame
     void Update()
@@ -16,18 +27,18 @@ public class BackgroundController : MonoBehaviour
 
     private void _Reset()
     {
-        transform.position = new Vector3(0.0f, verticalBoundary);
+        transform.position = new Vector3(horizontalBoundary, 0.0f);
     }
 
     private void _Move()
     {
-        transform.position -= new Vector3(0.0f, verticalSpeed) * Time.deltaTime;
+        transform.position -= new Vector3(horizontalSpeed, 0.0f) * Time.deltaTime;
     }
 
     private void _CheckBounds()
     {
         // if the background is lower than the bottom of the screen then reset
-        if (transform.position.y <= -verticalBoundary)
+        if (transform.position.x <= -horizontalBoundary)
         {
             _Reset();
         }
